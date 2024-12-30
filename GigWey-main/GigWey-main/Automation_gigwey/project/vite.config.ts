1612709cdb9304de946c94313ce6272fbd3f2ead
Gnanesh-16 <gnaneshbalusa016g@gmail.com>
2024-12-31 01:43:19 +0530

    ADD hike

--- GigWey-main/GigWey-main/Automation_gigwey/project/vite.config.ts
+++ GigWey-main/GigWey-main/Automation_gigwey/project/vite.config.ts
@@ -4,9 +4,9 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/book-landing-page/",
+  base: "/Gigwey-/",
   optimizeDeps: {
     exclude: ['lucide-react'],
-    
+
   },
 });
